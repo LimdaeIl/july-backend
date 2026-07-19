5467be6881402fc693eee973d240831745b7300b
LimdaeIl <optional94@naver.com>
2026-07-19 18:46:44 +0900

    ✨#35: 구현된 필터들을 Security Config 등록

--- src/main/java/com/backend/july/auth/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/backend/july/auth/infrastructure/security/config/SecurityConfig.java
@@ -5,20 +5,32 @@ import static com.backend.july.auth.infrastructure.security.config.SecurityEndpo
 import static com.backend.july.auth.infrastructure.security.config.SecurityEndpoints.AUTH_PUBLIC;
 import static com.backend.july.auth.infrastructure.security.config.SecurityEndpoints.SYSTEM_PUBLIC;
 
+import com.backend.july.auth.infrastructure.security.filter.JwtAccessDeniedHandler;
+import com.backend.july.auth.infrastructure.security.filter.JwtAuthenticationEntryPoint;
+import com.backend.july.auth.infrastructure.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,7 +41,10 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(AUTH_PUBLIC).permitAll()
@@ -37,6 +52,7 @@ public class SecurityConfig {
                         .requestMatchers(SYSTEM_PUBLIC).permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
