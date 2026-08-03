45a285765d192d881c65918218ffa5afd6275fcc
LimdaeIl <optional94@naver.com>
2026-08-03 18:46:54 +0900

    ♻️#63: 시간대를 서울로 통일

--- src/main/java/com/backend/july/common/config/TimeConfig.java
+++ src/main/java/com/backend/july/common/config/TimeConfig.java
@@ -1,11 +1,15 @@
 package com.backend.july.common.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TimeConfig {
+
+    private static final ZoneId SERVICE_ZONE =
+            ZoneId.of("Asia/Seoul");
 
     @Bean
     public Clock clock() {
